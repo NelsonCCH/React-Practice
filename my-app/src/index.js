3df4738ddb09f5ead527c8699364ece8cbbd459d
Nelson Chan <nelson12330@gmail.com>
2019-07-15 15:12:22 +0800

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,8 +15,6 @@ function Square(props){
   
   class Board extends React.Component {
 
-
-
     renderSquare(i) {
       return (<Square 
       value={this.props.squares[i]}
@@ -55,14 +53,14 @@ function Square(props){
         history: [{
           squares: Array(9).fill(null)
         }],
-        stepNUmber:0,
+        stepNumber:0,
         xIsNext: true
       }
-    }
+   }
 
     handleClick(i){
-      const history = this.state.history;
-      const current = history[history.length-1];
+      const history = this.state.history.slice(0, this.state.stepNumber +1);
+      const current = history[history.length -1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]){
         return;
@@ -72,20 +70,21 @@ function Square(props){
         history: history.concat([{
           squares: squares
         }]),
-        xIsNext: !this.state.xIsNext
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext,
       });
     }
 
     jumpTo(step)  {
       this.setState({
-        stepNUmber: step,
+        stepNumber: step,
         xIsNext: (step%2) === 0
       });
     }
 
     render() {
       const history = this.state.history;
-      const current = history[history.length-1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
